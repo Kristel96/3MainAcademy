8682e63f047c3f07b1dc6d5ea5643a40a01d04ce
Khrystyna Rymar <>
2019-07-06 12:31:59 +0300

    Added horoscope and random images api

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
-window.location.href = "views/checker.html";
-function showAudi (){
-    window.location.href = "views/audi.html";
+var mainHeader = document.getElementById("header");
+function getSignHoroscope(context){
+    console.log(context);
+    var sign = context.id;
+    var xhr = new XMLHttpRequest();
+    var url = "http://sandipbgt.com/theastrologer/api/horoscope/"+sign+"/today/";
+    xhr.open("GET", url, true);
+    xhr.onload = function(){
+        hideAllSigns();
+        var info = JSON.parse(xhr.responseText);
+        console.log(info);
+        var sunSign = document.getElementById("sunSign");
+        sunSign.innerText = info.sunsign;
+        var servCont = document.getElementById("serverContent");
+        servCont.innerText = info.horoscope;
+        var mood = document.getElementById("mood");
+        mood.innerText = info.meta.mood;
+        // https://source.unsplash.com/random
+        var randomImage = new XMLHttpRequest();
+        randomImage.open("GET", "https://source.unsplash.com/user/erondu/700x300", true);
+        randomImage.onload = function(){
+            var img = document.getElementById("randomImg");
+            img.src = randomImage.responseURL;
+          console.log(randomImage.responseURL);
+        };
+        randomImage.send();
+
+    };
+    xhr.send();
 }
-function showChevrolet() {
-    window.location.href = "views/chevrolet.html";
+function showAllSigns() {
+    var secondaryContainer = document.getElementById("secondaryCont");
+    secondaryContainer.style.display = "none";
+    var mainContainer = document.getElementById("mainCont");
+    mainContainer.style.display = "block";
+    mainHeader.innerText = " Choose your Sunsign! ";
 }
-function showBMW() {
-    window.location.href = "views/bmw.html";
-}
-function showOpel(){
-    window.location.href = "views/opel.html";
+function hideAllSigns() {
+    var secondaryContainer = document.getElementById("secondaryCont");
+    secondaryContainer.style.display = "block";
+    var mainContainer = document.getElementById("mainCont");
+    mainContainer.style.display = "none";
+    mainHeader.innerText= "You are lucky today!";
 }
