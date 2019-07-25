413ae9bb377b43fc474a7ed3bec39086d09cd928
Khrystyna Rymar <>
2019-07-25 19:03:51 +0300

    added promise function to horoscope api

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ var mainHeader = document.getElementById("header");
 function getSignHoroscope(context){
     console.log(context);
     var sign = context.id;
-    var xhr = new XMLHttpRequest();
-    var url = "http://sandipbgt.com/theastrologer/api/horoscope/"+sign+"/today/";
-    xhr.open("GET", url, true);
-    xhr.onload = function(){
+
+    get("http://sandipbgt.com/theastrologer/api/horoscope/"+sign+"/today/").then(function(text) {
         hideAllSigns();
-        var info = JSON.parse(xhr.responseText);
+        var info = JSON.parse(text.response);
         console.log(info);
         var sunSign = document.getElementById("sunSign");
         sunSign.innerText = info.sunsign;
@@ -15,19 +13,61 @@ function getSignHoroscope(context){
         servCont.innerText = info.horoscope;
         var mood = document.getElementById("mood");
         mood.innerText = info.meta.mood;
-        // https://source.unsplash.com/random
-        var randomImage = new XMLHttpRequest();
-        randomImage.open("GET", "https://source.unsplash.com/user/erondu/700x300", true);
-        randomImage.onload = function(){
-            var img = document.getElementById("randomImg");
-            img.src = randomImage.responseURL;
-          console.log(randomImage.responseURL);
-        };
-        randomImage.send();
+    }, function(error) {
+        console.log("Error!!!");
+        console.log(error);
+    });
+    get("https://source.unsplash.com/user/erondu/700x300").then(function(text) {
+        var img = document.getElementById("randomImg");
+        img.src = text.responseURL;
+    }, function(error) {
+        console.log("Error!!!");
+        console.log(error);
+    });
 
-    };
-    xhr.send();
+    // var xhr = new XMLHttpRequest();
+    // var url = "http://sandipbgt.com/theastrologer/api/horoscope/"+sign+"/today/";
+    // xhr.open("GET", url, true);
+    // xhr.onload = function(){
+    //     hideAllSigns();
+    //     var info = JSON.parse(xhr.responseText);
+    //     console.log(info);
+    //     var sunSign = document.getElementById("sunSign");
+    //     sunSign.innerText = info.sunsign;
+    //     var servCont = document.getElementById("serverContent");
+    //     servCont.innerText = info.horoscope;
+    //     var mood = document.getElementById("mood");
+    //     mood.innerText = info.meta.mood;
+    //     // https://source.unsplash.com/random
+    //     var randomImage = new XMLHttpRequest();
+    //     randomImage.open("GET", "https://source.unsplash.com/user/erondu/700x300", true);
+    //     randomImage.onload = function(){
+    //         var img = document.getElementById("randomImg");
+    //         img.src = randomImage.responseURL;
+    //       console.log(randomImage.responseURL);
+    //     };
+    //     randomImage.send();
+    //
+    // };
+    // xhr.send();
 }
+function get(url) {
+    return new Promise(function(succeed, fail) {
+        var request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.addEventListener("load", function() {
+            if (request.status < 400)
+                succeed(request);
+            else
+                fail(new Error("Request failed: " + request.statusText));
+        });
+        request.addEventListener("error", function() {
+            fail(new Error("Network error"));
+        });
+        request.send();
+    });
+}
+
 function showAllSigns() {
     var secondaryContainer = document.getElementById("secondaryCont");
     secondaryContainer.style.display = "none";
